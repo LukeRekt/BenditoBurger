d39ab047ba0c9f4684ad76bf83153efb1688c5bc
LukeRekt <lucasrsl12@gmail.com>
2023-05-28 14:36:28 -0300

    alinhamentos corrigidos

--- src/components/CarrinhoItems/CarrinhoItems.js
+++ src/components/CarrinhoItems/CarrinhoItems.js
@@ -3,6 +3,7 @@ import { AiFillPlusCircle } from 'react-icons/ai';
 export default function CarrinhoItems(props){
     return(
         <div className={styles.container}>
+            <div className={styles.burgerContent}>
             <img src={props.imagem}/>
             <div className={styles.cartText}>
                 <p className={styles.nomeSanduiche}>{props.nome}</p>
@@ -10,6 +11,8 @@ export default function CarrinhoItems(props){
                 <p>R$ {props.preco}</p>
                 <p>1x</p>
             </div>
+                 </div>
+
             <div className={styles.seletorQuantidade}>
                 <AiFillPlusCircle/>
                 <p>{props.quantidade}</p>
